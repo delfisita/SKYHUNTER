14be883861d0a81100510b3b629b4d3c10acd694
Alejandro Nicolas Biga <131388243+delfisita@users.noreply.github.com>
2025-06-17 17:10:28 -0300

    AJUSTE PLAYER ESQUIVE

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,43 @@
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 7f;
     private int moveDirection = 0;
+    private float initialPositionX;
 
     private PlayerController controller;
 
     private void Start()
     {
+       
+        initialPositionX = transform.position.x;
         controller = GetComponent<PlayerController>();
     }
 
     private void Update()
     {
-        Vector3 movement = new Vector3(moveDirection, 0, 0) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
-    }
+        if (controller.isShooter) return;
 
+        if (moveDirection != 0)
+        {
+           
+            Vector3 movement = new Vector3(moveDirection, 0, 0) * moveSpeed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+        else
+        {
+          
+            float newX = Mathf.Lerp(transform.position.x, initialPositionX, 2f * Time.deltaTime);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+    }
     public void MoveLeft()
     {
-        if (controller.isShooter) return; // no se mueve si está disparando
+        if (controller.isShooter) return;
         moveDirection = -1;
     }
-
     public void MoveRight()
     {
         if (controller.isShooter) return;
